129fb04db6df1da7e192a92ed797666ffd6089a0
Vikask2612 <133359828+Vikask2612@users.noreply.github.com>
2025-03-26 18:38:27 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css';
+import AddTodo from './app/components/AddTodo';
+import Todos from './app/components/Todos'
+
+
+function App() {
+
+
+  return (
+    <>
+     <h1>Learn about redux toolkit</h1>
+     <AddTodo/>
+     <Todos/>
+    </>
+  )
+}
+
+export default App
